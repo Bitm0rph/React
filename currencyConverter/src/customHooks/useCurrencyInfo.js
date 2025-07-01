6a96a2a66b8d4d098a15ef41acb78d1bbd72163f
tig-paul <166623682+tig-paul@users.noreply.github.com>
2025-07-01 17:46:33 +0530

    updated currencyConverter

--- currencyConverter/src/customHooks/useCurrencyInfo.js
+++ currencyConverter/src/customHooks/useCurrencyInfo.js
@@ -3,9 +3,9 @@ import { useEffect, useState } from "react";
 function useCurrencyInfo(currency){
     const [data,setData] = useState({})
     useEffect(() => {
-        fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
+        fetch(`https://open.er-api.com/v6/latest/${currency}`)
         .then((res) => res.json())
-        .then((res) => setData(res[currency]))
+        .then((res) => setData(res.rates))
     },[currency])
     return data;
 }
